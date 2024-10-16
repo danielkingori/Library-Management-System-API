5e848e2de2063db1fb86016e2b04b68d19674693
danielkingori <kingndiritu@gmail.com>
2024-10-16 17:11:29 +0300

    custom admin interface

--- LMS_API/books/admin.py
+++ LMS_API/books/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Author, Book, BorrowRecord
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BorrowRecord)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn', 'published_date', 'number_of_copies_available')
+
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'borrow_date', 'return_date', 'status')
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BorrowRecord, BorrowRecordAdmin)
--- LMS_API/users/admin.py
+++ LMS_API/users/admin.py
@@ -1,30 +1,16 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from .models import CustomUser
-
-# # Register CustomUser with UserAdmin
-# class CustomUserAdmin(UserAdmin):
-#     # Display the role in the admin list view
-#     list_display = ('username', 'email', 'role', 'is_active', 'date_of_membership')
-#     fieldsets = UserAdmin.fieldsets + (
-#         (None, {'fields': ('role', 'date_of_membership', 'is_active')}),
-#     )
-# admin.site.register(CustomUser,UserAdmin,CustomUserAdmin)
-
-# Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register CustomUser with UserAdmin
 class CustomUserAdmin(UserAdmin):
     # Display the role in the admin list view
     list_display = ('username', 'email', 'role', 'is_active', 'date_of_membership')
     
-    # Copy the existing fieldsets and add new fields where necessary
     fieldsets = (
-        *UserAdmin.fieldsets,  # Unpack the default fieldsets from UserAdmin
-        (None, {'fields': ('role',)}),  # Add your custom fields here
+        #Unpack the default fieldsets from UserAdmin
+        *UserAdmin.fieldsets,  
+        # Add your custom field of role
+        (None, {'fields': ('role',)}), 
     )
 
 # Register the CustomUser model with CustomUserAdmin
