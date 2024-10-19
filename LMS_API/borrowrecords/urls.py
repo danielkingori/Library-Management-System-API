93d6290d08241a2491dc9b1df21a8bc3c0c06519
danielkingori <kingndiritu@gmail.com>
2024-10-19 22:55:25 +0300

    read me updates

--- LMS_API/borrowrecords/urls.py
+++ LMS_API/borrowrecords/urls.py
@@ -5,7 +5,6 @@ from .views import borrow_history, borrow_book, return_book
 
 urlpatterns = [
     path('borrow/', borrow_book, name='borrow-book'),
-    path('return/', return_book, name='return-book'),
     path('return/<int:book_id>/', return_book, name='return-book'),
     path('history/', borrow_history, name='borrowing-history'),
 ]
